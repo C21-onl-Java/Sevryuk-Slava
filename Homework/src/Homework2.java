6c6c40870a532cab50339a187a74554946b02dec
Slava <76904504+bnq7@users.noreply.github.com>
2023-04-10 14:42:28 +0300

    Add files via upload

---
+++ Homework/src/Homework2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя:");
+        String string = scanner.nextLine();
+        System.out.println("Ваше Имя:"+string);
+
+    }
+}
+
+
+
